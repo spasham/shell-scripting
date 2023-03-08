e1b320a23a149fbec3f04f1b6bdda20e6048a239
CodingManoj <mvp2612@hotmail.com>
2023-03-07 21:41:58 -0500

    AutoPush

--- 03-colors.sh
+++ 03-colors.sh
@@ -21,3 +21,6 @@
  #      echo -e "\e[32m I am printing Green Color \e[0m"
 
 echo -e "\e[32m I am printing Green Color \e[0m"
+echo -e "\e[31m I am printing Red Color \e[0m"
+echo -e "\e[33m I am printing Yellow Color \e[0m"
+echo -e "\e[34m I am printing Blue Color \e[0m"
