61d8629fac295b22033bec92bc2e2b062467ab26
CodingManoj <mvp2612@hotmail.com>
2023-03-07 22:17:37 -0500

    AutoPush

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -4,4 +4,5 @@ echo $0    # Prints Script Name
 echo $1    # Takes the first value from the command line 
 echo $2    # Takes the second value from the command line 
 
-echo $*
+echo $*    # $* is going to print the used variables  
+echo $@ 
