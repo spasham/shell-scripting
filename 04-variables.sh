d7c98c0372242bfdbf2e4fc174422ac9341965f9
CodingManoj <mvp2612@hotmail.com>
2023-03-07 21:56:33 -0500

    AutoPush

--- 04-variables.sh
+++ 04-variables.sh
@@ -22,3 +22,6 @@ echo "I am pritning the value of d $d"
 # When you try to print a variable which is not declared, bash is going to consider that as Null or empty
 
 # rm -rf /data/${DATA_DIR}   # /data/test  ---> rm -rf /data/
+
+# How do you supply variables from the command line 
+# export varName =  value 
---
+++ 05-dynamic-vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+TODAY-DATE="03-07-2023"
+
+echo "Welcome to Bash Training, Today date is ${TODAY-DATE}"
