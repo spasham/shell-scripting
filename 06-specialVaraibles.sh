d3f317c0b8f5da631694856d08601e8a0bca247d
CodingManoj <mvp2612@hotmail.com>
2023-03-07 22:09:53 -0500

    AutoPush

--- 06-specialVaraibles.sh
+++ 06-specialVaraibles.sh
@@ -3,3 +3,9 @@
 # Special Characters :  $0 to $9 , $* , $# , #@ , $$   
 
 echo "Name of the script is : $0"     # Gives the name of the script you're running  
+
+echo first value is $1
+echo second value is $2 
+echo third value is $3
+
+# bash scriptName.sh 100 200 300 
