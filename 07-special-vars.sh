f9ee1c864521042a7da0e664ea19de4a0eb01362
CodingManoj <mvp2612@hotmail.com>
2023-03-07 22:19:29 -0500

    AutoPush

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -5,4 +5,8 @@ echo $1    # Takes the first value from the command line
 echo $2    # Takes the second value from the command line 
 
 echo $*    # $* is going to print the used variables  
-echo $@ 
+echo $@    # $@ is going to print the used variables  
+echo $$    # $$ is going to print the PID of the current proces 
+echo $#    # $# is going to pring the number of arguments
+
+# $* or $@ both of the prints the used variables in the scirpt
