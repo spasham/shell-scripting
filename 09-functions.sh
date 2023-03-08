36287c17a7045a341a40408878a2a49b137a7329
CodingManoj <mvp2612@hotmail.com>
2023-03-07 22:30:19 -0500

    AutoPush

---
+++ 09-functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# There are 4 types of command available : 
+
+# 1) Binary                   ( /bin  , /sbin )
+# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# 3) Shell Built-in Commands  
+# 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
+
+# Declaring a function 
+
+sample() {
+    echo "I am a messaged called from sample function 
+}
+
+# This is how we call a function.
+
+sample 
