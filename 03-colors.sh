8097c8ea6da0c0f98fff050f67755b87debd32a8
CodingManoj <mvp2612@hotmail.com>
2023-03-07 21:44:08 -0500

    AutoPush

--- 03-colors.sh
+++ 03-colors.sh
@@ -24,3 +24,8 @@ echo -e "\e[32m I am printing Green Color \e[0m"
 echo -e "\e[31m I am printing Red Color \e[0m"
 echo -e "\e[33m I am printing Yellow Color \e[0m"
 echo -e "\e[34m I am printing Blue Color \e[0m"
+
+# How to print a background color 
+ #      echo -e "\e[BackGroundCOL-CODE;ForeGroundColorm  Your Message To Be Printed \e[0m"
+
+echo -e "\e[43;32m I am printing Green Color with YELLOW as Background \e[0m"
