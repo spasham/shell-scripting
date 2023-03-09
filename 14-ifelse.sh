2d038d6c9cf575be72e83924ffe84d08b5b82633
CodingManoj <mvp2612@hotmail.com>
2023-03-08 22:02:15 -0500

    AutoPush

--- 14-ifelse.sh
+++ 14-ifelse.sh
@@ -61,8 +61,14 @@ ACTION=$1
 
 if [ "$ACTION" == "start" ]; then 
     echo -e "\e[32m Service Payment is Starting \e[0m" 
+    exit 0
+
+elif [ "$ACTION" == "stop" ]; then 
+    echo -e "\e[31m Service Payment is stopping \e[0m" 
+    exit 1
 
 else 
-    echo -e "\e[31m Service Payment Status is unknown \e[0m"
+    echo -e "\e[33m Service Payment Status is unknown \e[0m"
+    exit 3
 
 fi 
