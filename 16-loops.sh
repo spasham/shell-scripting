da066aa2c967dd2db2ddbec038586f03bc468694
CodingManoj <mvp2612@hotmail.com>
2023-03-08 22:10:05 -0500

    AutoPush

---
+++ 16-loops.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+# What is Loop ? 
+# ---> If you want to run something for certain number of times, we will use a loop.
+
+# There are 2 major types of loops ; 
+#     1) For loop       ( When you know something to be executed n number of time, we use for loop)
+#     2) While loop     ( When you don't know something to be executed n number of times, we use while loop)
+
+
+# For Loop Syntax:
+
+for val in 10 20 30 40 50 ; do 
+    echo $val 
+done 
