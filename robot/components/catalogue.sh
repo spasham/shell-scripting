64e4d57d03698f8af355c84e51ad99fcb87af052
CodingManoj <mvp2612@hotmail.com>
2023-03-09 22:26:00 -0500

    AutoPush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -54,17 +54,20 @@ mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
 chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
 stat $?
 
-# 1. So let's switch to the `roboshop` user and run the following commands.
+echo -n "Installing the $COMPONENT Application :"
+cd /home/$APPUSER/$COMPONENT/ 
+npm install &>> $LOGFILE
+stat $? 
 
-# ```bash
-# $ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
-# $ cd /home/roboshop
-# $ unzip /tmp/catalogue.zip
-# $ mv catalogue-main catalogue
-# $ cd /home/roboshop/catalogue
-# $ npm install
+echo -n "Updating the systemd file with DB Details :"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+stat $? 
 
-# ```
+echo -n "Starting the $COMPONENT service : "
+systemctl daemon-reload &>> $LOGFILE
+systemctl enable $COMPONENT &>> $LOGFILE
+systemctl start $COMPONENT &>> $LOGFILE
 
 # 1. Update SystemD file with correct IP addresses
     
