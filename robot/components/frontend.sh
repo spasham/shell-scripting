af0491151db248dfd48f7e72bd02d785bc7dd08e
CodingManoj <mvp2612@hotmail.com>
2023-03-09 21:41:33 -0500

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -10,34 +10,27 @@ if [ "$ID" -ne 0 ] ; then
     exit 1
 fi 
 
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+        exit 2
+    fi 
+}
+
 echo -n "Installing Ngnix : "
 yum install nginx -y &>> /tmp/frontend.log
-
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi 
+stat $? 
 
 echo -n "Downloading the frontend component :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi 
+stat $? 
 
 echo -n "Performing Cleanup of Old Frontend Content :"
 cd /usr/share/nginx/html
 rm -rf *  &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi 
+stat $? 
 
 echo -n "Copying the downloaded frontend content: "
 unzip /tmp/frontend.zip  &>> /tmp/frontend.log 
@@ -46,19 +39,9 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi 
+stat $? 
 
 echo -n "Starting the service: "
 systemctl enable nginx  &>> /tmp/frontend.log
 systemctl restart nginx &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then 
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi 
+stat $? 
