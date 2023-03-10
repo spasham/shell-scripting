f531b836dab958d0ff6f5a4f46060a73c36fe3c6
CodingManoj <mvp2612@hotmail.com>
2023-03-09 21:37:42 -0500

    AutoPush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -11,30 +11,54 @@ if [ "$ID" -ne 0 ] ; then
 fi 
 
 echo -n "Installing Ngnix : "
-yum install ngasdfsadfsdfinx -y &>> /tmp/frontend.log
+yum install nginx -y &>> /tmp/frontend.log
 
 if [ $? -eq 0 ] ; then 
     echo -e "\e[32m Success \e[0m"
 else 
     echo -e "\e[31m Failure \e[0m"
+    exit 2
 fi 
 
+echo -n "Downloading the frontend component :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi 
 
+echo -n "Performing Cleanup of Old Frontend Content :"
 cd /usr/share/nginx/html
 rm -rf *  &>> /tmp/frontend.log
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi 
+
+echo -n "Copying the downloaded frontend content: "
 unzip /tmp/frontend.zip  &>> /tmp/frontend.log 
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi 
+
+echo -n "Starting the service: "
 systemctl enable nginx  &>> /tmp/frontend.log
 systemctl restart nginx &>> /tmp/frontend.log
-
-
-
-# # Here are my 3 observations :
-# 1) Few of the steps are failed, still my script executed irrespective of the failure.  : set -e 
-# 2) Installation failed because I've not validated that I've root privileges 
-# 3) The information I would like to provide is like Sucess or Failure
+if [ $? -eq 0 ] ; then 
+    echo -e "\e[32m Success \e[0m"
+else 
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi 
