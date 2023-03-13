eb1811fb3a5df21a62b51bca12ceaeb2a0ebf78c
CodingManoj <mvp2612@hotmail.com>
2023-03-12 22:47:15 -0400

    AutoPush

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -61,7 +61,7 @@ NPM_INSTALL() {
 CONFIG_SVC() {
 
     echo -n "Updating the systemd file with DB Details :"
-    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    sed -i  -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
     stat $? 
 
