ef496253025d0ad435a5a088b4e9cac8277d44ee
CodingManoj <mvp2612@hotmail.com>
2023-03-12 22:55:48 -0400

    AutoPush

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -61,7 +61,7 @@ NPM_INSTALL() {
 CONFIG_SVC() {
 
     echo -n "Updating the systemd file with DB Details :"
-    sed -i  -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    sed -i -e  's/DBHOST/mysql.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
     mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
     stat $? 
 
@@ -72,6 +72,38 @@ CONFIG_SVC() {
     stat $?
 
 }
+
+MVN_PACKAGE() {
+    echo -n "Creating the $COMPONENT Package :"
+    cd /home/$APPUSER/$COMPONENT/ 
+    mvn clean package  &>> $LOGFILE
+    mv target/shipping-1.0.jar shipping.jar
+    stat $?   
+}
+
+JAVA() {
+    echo -n "Installing Maven  :" 
+    yum install java -y &>> $LOGFILE
+    stat $?
+
+    # Calling Create-User Functon 
+    CREATE_USER
+
+    # Calling Download_And_Extract Function
+    DOWNLOAD_AND_EXTRACT
+
+    # Calling Maven Package Functon
+    MVN_PACKAGE
+
+    # Calling Config-Svc Function
+    CONFIG_SVC
+
+}
+
+
+
+
+
 
 NODEJS() {
 
