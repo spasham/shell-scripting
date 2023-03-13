3c80baec6364b299079b7bd34d694819b8003912
CodingManoj <mvp2612@hotmail.com>
2023-03-12 22:47:24 -0400

    AutoPush

--- robot/components/cart.sh
+++ robot/components/cart.sh
@@ -4,10 +4,3 @@ COMPONENT=user
 source components/common.sh    # Source is going to load the file, so that you can call all of them as per your need
 
 NODEJS                         # Calling NodeJS Function.
-
-
-echo -n "Updating the systemd file with DB Details :"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $? 
-
