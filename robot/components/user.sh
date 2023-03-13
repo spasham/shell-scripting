b7efa1d3cf887de627c32addf50c7663c4f4665b
CodingManoj <mvp2612@hotmail.com>
2023-03-12 22:44:34 -0400

    AutoPush

--- robot/components/user.sh
+++ robot/components/user.sh
@@ -1,71 +1,13 @@
 #!/bin/bash 
 
-# set -e 
-
 COMPONENT=user
-LOGFILE="/tmp/$COMPONENT.log"
-APPUSER=roboshop
+source components/common.sh    # Source is going to load the file, so that you can call all of them as per your need
 
-# Validting whether the executed user is a root user or not 
-ID=$(id -u)
+NODEJS                         # Calling NodeJS Function.
 
-if [ "$ID" -ne 0 ] ; then 
-    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
-    exit 1
-fi 
-
-stat() {
-    if [ $1 -eq 0 ] ; then 
-        echo -e "\e[32m Success \e[0m"
-    else 
-        echo -e "\e[31m Failure \e[0m"
-        exit 2
-    fi 
-}
-
-
-echo -n "Configuring the nodejs repo :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>> $LOGFILE
-stat $? 
-
-echo -n "Installing NodeJS :"
-yum install nodejs -y &>> $LOGFILE
-stat $?
-
-id $APPUSER  &>> $LOGFILE
-if [ $? -ne 0 ] ; then 
-    echo -n "Creating the Application User Account :" 
-    useradd roboshop &>> $LOGFILE
-    stat $? 
-fi 
-
-echo -n "Downloading the $COMPONENT component :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $? 
-
-echo -n "Extracting the $COMPONENT in the $APPUSER directory"
-cd /home/$APPUSER 
-rm -rf /home/$APPUSER/$COMPONENT &>> $LOGFILE
-unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-stat $? 
-
-echo -n "Configuring the permissions :"
-mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
-chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
-stat $?
-
-echo -n "Installing the $COMPONENT Application :"
-cd /home/$APPUSER/$COMPONENT/ 
-npm install &>> $LOGFILE
-stat $? 
 
 echo -n "Updating the systemd file with DB Details :"
 sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
 mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 stat $? 
 
-echo -n "Starting the $COMPONENT service : "
-systemctl daemon-reload &>> $LOGFILE
-systemctl enable $COMPONENT &>> $LOGFILE
-systemctl restart $COMPONENT &>> $LOGFILE
-stat $?
