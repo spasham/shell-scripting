e84d80fa9ee671f4347673cd8c298f8ea2529c84
CodingManoj <mvp2612@hotmail.com>
2023-03-13 23:05:33 -0400

    AutoPush

--- robot/components/payment.sh
+++ robot/components/payment.sh
@@ -3,5 +3,5 @@
 COMPONENT=payment
 source components/common.sh    # Source is going to load the file, so that you can call all of them as per your need
 
-NODEJS                         # Calling NodeJS Function.
+PYTHON                         # Calling NodeJS Function.
 
