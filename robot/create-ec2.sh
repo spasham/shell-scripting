8d4e9d540acdc89428adcec950fca9848017cab4
CodingManoj <mvp2612@hotmail.com>
2023-03-15 22:48:23 -0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -21,7 +21,7 @@ IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
                     --instance-type t3.micro \
                     --security-group-ids ${SGID} \
                     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress[]' | sed -e 's/"//g')
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 echo $IPADDRESS
 sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/r53.json
