481266938bf903150e0b55ecda083334c0f53c89
CodingManoj <mvp2612@hotmail.com>
2023-03-15 23:06:08 -0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -24,5 +24,6 @@ IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
                     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 echo $IPADDRESS
+ls -ltr 
 sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/r53.json
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
