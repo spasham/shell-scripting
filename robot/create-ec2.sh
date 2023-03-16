ca32def2f66626ea993fc03cba23ef5e11d010ef
CodingManoj <mvp2612@hotmail.com>
2023-03-15 23:10:51 -0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -14,7 +14,7 @@ COMPONENT=$1
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all-sg  | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 
-echo Ami ID is $AMI_ID
+echo -n "Ami ID is $AMI_ID"
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
 IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
@@ -23,7 +23,5 @@ IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
                     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
                     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo $IPADDRESS
-ls -ltr 
-sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/r53.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json | jq 
+sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq 
