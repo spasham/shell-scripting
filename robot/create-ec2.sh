0f46922926be05b83fde3a80dc326ab457d5ed1b
CodingManoj <mvp2612@hotmail.com>
2023-03-22 22:10:54 -0400

    AutoPush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -17,11 +17,31 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-all
 echo -n "Ami ID is $AMI_ID"
 
 echo -n "Launching the instance with $AMI_ID as AMI :"
-IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
-                --instance-type t3.micro \
-                --security-group-ids ${SGID} \
-                --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq 
+create_server() {
+    
+    echo "*** Launching $COMPONENT Server ***"
+
+    IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
+                    --instance-type t3.micro \
+                    --security-group-ids ${SGID} \
+                    --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+    sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" robot/record.json > /tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/record.json | jq
+
+    echo "*** $COMPONENT Server Completed ***"
+
+}
+
+if [ "$1" == "all" ] ; then 
+    for component in frontend mongodb catalogue cart user mysql redis rabbitmq shipping payment ; do  
+        COMPONENT=$component 
+        create_server
+    done
+
+else 
+        create_server
+
+fi 
