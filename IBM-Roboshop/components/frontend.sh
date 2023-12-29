53d94e56055481bfdbcabd848070d3e19da8dc78
Shiva Kumar Pasham <spasham@L-HYDWG8YMD6M-M.local>
2023-12-29 17:21:23 +0530

    added IBM-Roboshop components

---
+++ IBM-Roboshop/components/frontend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "I'm frontend"
+
+yum install nginx -y
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemct start nginx
+systemctl enable nginx
