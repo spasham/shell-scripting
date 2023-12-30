c8cf8c43d741cb50bca49332346dc91f3231d6e5
Shiva Kumar Pasham <spasham@L-HYDWG8YMD6M-M.local>
2023-12-30 14:12:23 +0530

    added IBM-Roboshop components

--- IBM-Roboshop/components/frontend.sh
+++ IBM-Roboshop/components/frontend.sh
@@ -9,6 +9,7 @@ cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
+mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
