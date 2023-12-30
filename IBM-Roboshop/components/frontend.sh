0cc3336101c7a379d221b00e6e1c2d4a5b7e8a33
Shiva Kumar Pasham <spasham@L-HYDWG8YMD6M-M.local>
2023-12-30 14:34:48 +0530

    updated script

--- IBM-Roboshop/components/frontend.sh
+++ IBM-Roboshop/components/frontend.sh
@@ -13,17 +13,17 @@ if [ $USER_ID -ne 0 ]; then
     exit 1
 fi
 
-yum install nginx -y
+yum install nginx -y &>>/tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html  
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf * &>>/tmp/frontend.log
+unzip /tmp/frontend.zip &>>/tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl start nginx
-systemctl enable nginx
+systemctl start nginx &>>/tmp/frontend.log
+systemctl enable nginx &>>/tmp/frontend.log
