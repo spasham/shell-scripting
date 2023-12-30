b38574276ca619f8874c4e0110056b15f20dfe04
Shiva Kumar Pasham <spasham@L-HYDWG8YMD6M-M.local>
2023-12-30 14:08:50 +0530

    added IBM-Roboshop components

--- IBM-Roboshop/components/frontend.sh
+++ IBM-Roboshop/components/frontend.sh
@@ -5,12 +5,12 @@ yum install nginx -y
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html  
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemct start nginx
+systemctl start nginx
 systemctl enable nginx
