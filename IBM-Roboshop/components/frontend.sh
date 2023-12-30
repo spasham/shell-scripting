adfc3ed03fb3ae4200858c3ae72c030a0bbdddec
Shiva Kumar Pasham <spasham@L-HYDWG8YMD6M-M.local>
2023-12-30 14:51:49 +0530

    updated script

--- IBM-Roboshop/components/frontend.sh
+++ IBM-Roboshop/components/frontend.sh
@@ -14,21 +14,53 @@ if [ $USER_ID -ne 0 ]; then
 fi
 
 echo -n "Installing nginx: "
-yum install ng-inx -y &>>/tmp/frontend.log
+yum install nginx -y &>>/tmp/frontend.log
 if [ $? -eq 0 ]; then
     echo "Success"
 else
     echo -e "\e[31m Failure \e[0m"
+    exit 2
 fi
+
+echo "Downling the frontend code: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
+echo "Performing cleanup of old frontend conetnet:"
 cd /usr/share/nginx/html  
 rm -rf * &>>/tmp/frontend.log
+
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
+
+echo "Copying the frontend code:"
 unzip /tmp/frontend.zip &>>/tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
 
+echo "Starting and enabling the service:"
 systemctl start nginx &>>/tmp/frontend.log
 systemctl enable nginx &>>/tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo -e "\e[31m Failure \e[0m"
+    exit 2
+fi
